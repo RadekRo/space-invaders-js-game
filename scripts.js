dad2b976b765d613b320a25c9f572caf23d22af4
Radek Rocławski <r.roclawski@ankara.pl>
2023-05-23 10:01:08 +0200

    Moved all the numeric values from CSS to JS

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
-gameboard = document.getElementById("gamebord");
-spaceship = document.getElementById("spaceship");
-left_margin = 230
+const BOARD_WIDTH = 500;
+const BOARD_HEIGHT = 500;
+const SHIP_WIDTH = 40;
+const SHIP_HEIGHT = 20;
+const SHIP_FLOAT = 10;
+
+let gameboard = document.getElementById("gameboard");
+let spaceship = document.getElementById("spaceship");
+let left_margin = (BOARD_WIDTH - SHIP_WIDTH) / 2;
+
+gameboard.style.width = BOARD_WIDTH + 'px';
+gameboard.style.height = BOARD_HEIGHT + 'px';
+spaceship.style.width = SHIP_WIDTH + 'px';
+spaceship.style.height = SHIP_HEIGHT + 'px';
+spaceship.style.left = left_margin + 'px';
+spaceship.style.bottom = SHIP_FLOAT + 'px';
 
 document.addEventListener('keydown', control);
 
